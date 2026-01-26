925a1f58f74a08efa6306f28ebb9f9da2b12bffb
El-brayan502 <brayanphonkponk@gmail.com>
2026-01-26 08:04:06 -0600

    Neji updated ✅️

---
+++ plugins/sticker-sticker.js
@@ -0,0 +1,50 @@
+import { sticker } from '../lib/bibliotecas/sticker.js'
+import uploadFile from '../lib/bibliotecas/uploadFile.js'
+import uploadImage from '../lib/bibliotecas/uploadImage.js'
+import { webp2png } from '../lib/bibliotecas/webp2mp4.js'
+
+
+let handler = async (m, { conn, args }) => {
+let userId = m.sender
+let packstickers = global.db.data.users[userId] || {}
+let texto1 = packstickers.text1 || global.packsticker
+let texto2 = packstickers.text2 || global.packsticker2
+let stiker = false
+try {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+let txt = args.join(' ')
+
+if (/webp|image|video/g.test(mime) && q.download) {
+if (/video/.test(mime) && (q.msg || q).seconds > 16)
+return conn.reply(m.chat, '💙 El video no puede durar más de *15 segundos* para crear un sticker virtual perfecto ✨', m)
+let buffer = await q.download()
+await m.react('🎤')
+
+let marca = txt ? txt.split(/[\u2022|]/).map(part => part.trim()) : [texto1, texto2]
+stiker = await sticker(buffer, false, marca[0], marca[1])
+} else if (args[0] && isUrl(args[0])) {
+let buffer = await sticker(false, args[0], texto1, texto2)
+stiker = buffer
+} else {
+return conn.reply(m.chat, '💙 Por favor, envía una *imagen* o *video* para crear un hermoso sticker virtual ✨', m)
+}} catch (e) {
+await conn.reply(m.chat, '💫 ¡Gomen! Ocurrió un error en el escenario virtual: ' + e.message + ' ✨', m)
+await m.react('🎵')
+} finally {
+if (stiker) {
+conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+await m.react('💙')
+}}}
+
+handler.help = ['sticker']
+handler.tags = ['sticker']
+handler.command = ['s', 'sticker']
+handler.register = true
+
+export default handler
+
+const isUrl = (text) => {
+return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)(jpe?g|gif|png)/, 'gi'))
+}
+
---
+++ settings.js
@@ -0,0 +1,34 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import moment from 'moment-timezone'
+
+global.botNumber = '' 
+global.owner = [
+  ['573244278232', 'Neji ssj', true],
+  ['50231458537', 'BrayanX330', true]
+]
+global.mods = []
+global.prems = []
+global.suittag = ['50231458537']
+
+global.botname = '𝙽𝚊𝚐𝚒𝙱𝚘𝚝-𝙰𝚒'
+global.author = 'Brayan330'
+
+global.sessions = 'Sessions'
+global.jadi = 'JadiBots'
+global.moneda = 'dolares'
+global.multiplier = 60
+global.prefix = /^[./!#?]/
+
+global.channel = 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i'
+global.md = 'https://github.com/El-brayan502/NagiBotV3'
+global.ch = { id: '120363417186717632@newsletter' }
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.cyanBright(`✨ [CONFIG] Se han actualizado los ajustes del bot`))
+  import(`${file}?update=${Date.now()}`)
+})
