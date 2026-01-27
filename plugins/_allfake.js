f29fde5411ca87b0f6cf9b33bfeae3b363ad18b9
Soy Neji <brayanphonkponk@gmail.com>
2026-01-26 21:45:49 -0600

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -41,10 +41,16 @@ handler.all = async function (m, { conn }) {
     const time = moment.tz('America/Mexico_City').hour()
     global.saludo = time >= 5 && time < 12 ? '☀️ Buenos días' : time >= 12 && time < 18 ? '🌤️ Buenas tardes' : '🌙 Buenas noches'
 
+   global.fkontak = {
+        key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "status@broadcast" } : {}) },
+        message: { contactMessage: { displayName: m.pushName || 'Player', vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;${m.pushName || 'User'};;;\nFN:${m.pushName || 'User'}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Celular\nEND:VCARD` } }
+    }
+}
+
 export default handler
 
 const file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
     unwatchFile(file)
-    console.log('✨ Actualizado: _allfeke.js')
+    console.log('✨ Actualizado: _allfeke.js (Nagi Bot Edition)')
 })
