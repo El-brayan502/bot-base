4ec2d759a53345e6736da0dc8eb299145b47cb01
Soy Neji <brayanphonkponk@gmail.com>
2026-01-29 21:07:50 -0600

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -16,7 +16,7 @@ handler.all = async function (m, { conn }) {
     global.nombrecanal = '🍀 NAGI SEIISHIRO UPDATES 🍀'
 
     // --- 🎯 CONFIGURACIÓN DE CANAL (ADAPTADO) ---
-    global.rcanaldev = {
+    global.rcanal = {
       contextInfo: {
         isForwarded: true,
         forwardedNewsletterMessageInfo: { 
