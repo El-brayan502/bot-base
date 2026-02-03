a1e6ec85d7daa89c1ea226eb277c83433bf5148e
Soy Neji <brayanphonkponk@gmail.com>
2026-02-03 02:07:26 -0600

    Actualizar owner-cleartmp.js

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -10,7 +10,7 @@ watch
 } from 'fs'
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => { 
 
-conn.reply(m.chat, ` Realizado, ya se ha eliminado los archivos de la carpeta tmp`, m)
+conn.reply(m.chat, ` *Realizado, ya se ha eliminado los archivos de la carpeta tmp*`, m)
 
 const tmp = [tmpdir(), join(__dirname, '../tmp')]
 const filename = []
